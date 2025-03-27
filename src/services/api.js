a5ce17fa70354808208993baaa28db94169beff4
dustin1818 <dustinamoda18@gmail.com>
2025-03-27 21:10:17 +0800

    remove caching

--- src/services/api.js
+++ src/services/api.js
@@ -12,6 +12,9 @@ const apiClient = axios.create({
   baseURL,
   headers: {
     'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    Pragma: 'no-cache',
+    Expires: '0',
   },
 })
 
